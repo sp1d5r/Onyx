7a5d05fba1ac55d620fa4aafaf67a718ee8dfd4c
Elijah <aa1719@imperial.ac.uk>
2023-03-27 06:02:23 +0100

    Initial Commit

---
+++ Shared/Black_DiamondApp.swift
@@ -0,0 +1,17 @@
+//
+//  Black_DiamondApp.swift
+//  Shared
+//
+//  Created by Elijah Ahmad on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Black_DiamondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
